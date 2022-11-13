427d51bd2ea1761378e9d01535297213ce95905c
alpacaHyun <flaehdan@gmail.com>
2022-11-14 08:03:01 +0900

    type : feat Stack, Queue example
    body :

--- src/standardofjava/chapter11/EX20_StackQueue.java
+++ src/standardofjava/chapter11/EX20_StackQueue.java
@@ -3,6 +3,9 @@ package standardofjava.chapter11;
 public class EX20_StackQueue {
     public static void main(String[] args) {
 
-//        if(args)
+        if(args.length != 1){
+            System.out.println("Usage: java Ex11_3");
+            System.out.println("Example : jaca Ex");
+        }
     }
 }
