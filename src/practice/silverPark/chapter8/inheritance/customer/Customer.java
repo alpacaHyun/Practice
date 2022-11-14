ecedd62fd017d07c38216a509f1cc0f873e8711f
alpacaHyun <flaehdan@gmail.com>
2022-11-14 16:44:23 +0900

    type : feat 상속
    body :

---
+++ src/practice/silverPark/chapter8/inheritance/customer/Customer.java
@@ -0,0 +1,67 @@
+package practice.silverPark.chapter8.inheritance.customer;
+
+public class Customer {
+
+    private static final String SILVER = "SILVER";
+    private static final String GOLD = "GOLD";
+    private static final String VIP = "VIP";
+
+    protected int customerID;
+    protected String customerNmae;
+    protected String customerGrade;
+    int bonusPoint;  // private 이어도 생성이 된다. (접근이 안되는 것)
+    protected double bonusRatio;
+
+//    public Customer() {
+//        super(); // 모든 클래스의 최상위 클래스인 Object를 상속받았기 때문에 자동으로 들어감
+//        customerGrade = SILVER;
+//        bonusRatio = 0.01;
+//    }
+
+    public Customer(int customerID, String customerNmae){
+        this.customerID = customerID;
+        this.customerNmae = customerNmae;
+        customerGrade = "SILVER";
+        bonusRatio = 0.01;
+
+        System.out.println("Customer(int, String) 호출");
+    }
+
+
+    public int calcPrice(int price) {
+        if (customerGrade == SILVER) {
+            return bonusPoint += price * bonusRatio;
+        }
+        return price;
+    }
+
+    public String showCustomerInfo() {
+        return customerNmae + "님의 등급은 " + customerGrade + "이며, 보너스 포인트는 " + bonusPoint + "입니다.";
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID){
+        this.customerID = customerID;
+    }
+
+    public void setCustomerName(String customerName){
+        this.customerNmae = customerName;
+    }
+
+    public String getCustomerGrade(){
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade){
+        this.customerGrade = customerGrade;
+    }
+
+    public void setBonusPoint(int bonusPoint){
+        this.bonusPoint = bonusPoint;
+    }
+
+}
+
---
+++ src/practice/silverPark/chapter8/inheritance/customer/VIPCustomer.java
@@ -0,0 +1,23 @@
+package practice.silverPark.chapter8.inheritance.customer;
+
+public class VIPCustomer extends Customer {
+
+    private static final String VIP = "VIP";
+    private double saleRatio;
+    private int agentID;
+
+    public VIPCustomer(int customerID, String customerName) {
+        super(customerID, customerName);
+        ; // 입력해주지 않아도 원래 상위(부모)클래스가 자동으로 들어감(프리컴파일 단계에서)
+        customerGrade = VIP;
+        bonusRatio = 0.05;
+        saleRatio = 0.1;
+
+        System.out.println("VIPCustomer(int, String) 호출");
+    }
+
+    public int getAgentID() {
+        return agentID;
+    }
+
+}
