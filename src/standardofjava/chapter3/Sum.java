76fbfdc54ad13854b710eaf612e18af6e99a59c3
alpacaHyun <flaehdan@gmail.com>
2022-12-12 17:24:02 +0900

    type : 자바의 정석
    body :

---
+++ src/standardofjava/chapter3/Sum.java
@@ -0,0 +1,13 @@
+package standardofjava.chapter3;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        int j = 0;
+
+        System.out.println(i++);
+        System.out.println(++j);
+    }
+}
