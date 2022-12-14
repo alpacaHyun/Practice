7986b054028a16d5ffa4c271acfaffbecaaf5b6e
alpacaHyun <flaehdan@gmail.com>
2022-12-14 18:47:45 +0900

    type : 자바의 정석
    body :

---
+++ src/standardofjava/chapter11/Ex11_10.java
@@ -0,0 +1,25 @@
+package standardofjava.chapter11;
+
+import java.util.*;
+
+
+public class Ex11_10 {
+
+    public static void main(String[] args) {
+
+        Set set = new HashSet();
+
+        for (int i = 0; set.size() < 6; i++) {
+            int num = (int)(Math.random()*45) + 1;
+            set.add(new Integer(num));
+        }
+
+//        List list = new LinkedList(set);
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        System.out.println(list);
+
+
+
+    }
+}
