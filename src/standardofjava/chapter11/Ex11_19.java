62d0cc2d03e7c12c011ab9aa53136240f3670671
alpacaHyun <flaehdan@gmail.com>
2022-12-15 15:22:47 +0900

    type : 자바의 정석
    body :

---
+++ src/standardofjava/chapter11/Ex11_19.java
@@ -0,0 +1,51 @@
+package standardofjava.chapter11;
+
+import static java.util.Collections.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex11_19 {
+
+    public static void main(String[] args) {
+
+        List list = new ArrayList();
+        System.out.println(list);
+
+        addAll(list, 1,2,3,4,5);
+        System.out.println(list);
+
+        rotate(list, 2);  // 오른쪽으로 2만큼 이동
+        System.out.println(list);
+
+        swap(list, 0, 2);  // 첫번째와 세번째 교환
+        System.out.println(list);
+
+        shuffle(list); // 임의로 섞기
+        System.out.println(list);
+
+        sort(list, reverseOrder()); // 역순정렬
+        System.out.println(list);
+
+        sort(list);                 // 정렬
+        System.out.println(list);
+
+        int idx = binarySearch(list, 3); // 3이 저장된 위치 반환
+        System.out.println(idx);
+
+        System.out.println(max(list));
+        System.out.println(min(list));
+
+        fill(list, 9); // 9로 채운다
+        System.out.println(list);
+
+        List newList = nCopies(list.size(), 2); // list 와 같은 크기의 새로운 list 생성하고 2로 채운다, 단 결과는 변경 불가
+        System.out.println(newList);
+
+//        newList.add(1); // UnsupportedOperationException 발생
+
+        copy(list, newList);
+        System.out.println(list);
+
+    }
+}
