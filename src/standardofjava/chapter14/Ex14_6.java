094a894710056d7f83e36baa00a1a8aed7f56723
alpacaHyun <flaehdan@gmail.com>
2022-12-22 08:42:03 +0900

    feat : 자바의 정석
    body :

---
+++ src/standardofjava/chapter14/Ex14_6.java
@@ -0,0 +1,19 @@
+package standardofjava.chapter14;
+
+import static java.util.Optional.of;
+
+import java.io.*;
+import java.util.stream.*;
+
+public class Ex14_6 {
+
+    public static void main(String[] args) {
+
+        File[] fileArr = {new File("Ex1.java"), new File("Ex1.bak"), new File("Ex2.java"),
+        new File("Ex1"), new File("Ex1.txt")
+        };
+
+//        Stream<File> fileStream = Stream<File>.of(fileArr);
+
+    }
+}
